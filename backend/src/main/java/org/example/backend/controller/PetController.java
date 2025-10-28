01f5ae7afd1ed7310affa89a3bab1aaea389a2f1
skperry99 <sperry939@gmail.com>
2025-10-27 19:28:14 -0500

    Added PetController

--- backend/src/main/java/org/example/backend/controller/PetController.java
+++ backend/src/main/java/org/example/backend/controller/PetController.java
@@ -1,4 +1,27 @@
 package org.example.backend.controller;
 
+import org.example.backend.model.Pet;
+import org.example.backend.service.PetService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pets")
 public class PetController {
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @GetMapping
+    public List<Pet> getAllPets() {
+        return petService.getAllPets();
+    }
+
+    @PostMapping
+    public Pet createPet(@RequestBody Pet pet) {
+        return petService.savePet(pet);
+    }
 }
