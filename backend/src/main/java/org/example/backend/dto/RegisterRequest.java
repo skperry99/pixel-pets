a2777fec854818f21ba43137bd8fd3956eaa2689
skperry99 <sperry939@gmail.com>
2025-11-04 23:19:28 -0600

    Added RegisterRequest DTO

---
+++ backend/src/main/java/org/example/backend/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package org.example.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+
+}
