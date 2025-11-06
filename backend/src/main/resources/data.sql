6ce03b960419f72a80589a516a1425694e48fbef
skperry99 <sperry939@gmail.com>
2025-11-06 14:02:38 -0600

    Removed DB seed data (data.sql) and added     @ResponseStatus(HttpStatus.NO_CONTENT) to pet and user controllers

--- backend/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
--- Create sample user
-INSERT INTO users (user_name, email, password) VALUES
-    ('testuser', 'test@example.com', '$2a$10$Dow1.xf8gMERKQjHTh0DxON9sfMPGFnB0PvcMoVIKDVSDHWoJ3vPG');
-
--- Sample pets for sample user
-INSERT INTO pets (name, type, level, hunger, happiness, energy, user_id) VALUES
-    ('Pixel', 'Dog', 1, 100, 100, 100, 1),
-    ('Whiskers', 'Cat', 1, 100, 100, 100, 1),
-    ('Ember', 'Dragon', 1, 100, 100, 100, 1);
