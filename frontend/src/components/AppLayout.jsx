a30c418abdbee827c5950249244911eb9895d139
skperry99 <sperry939@gmail.com>
2025-11-06 23:42:57 -0600

    Added NavBar and updated AppLayout to include it

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,9 +1,11 @@
+import NavBar from "./NavBar";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function AppLayout({ headerProps, children }) {
   return (
     <>
+      <NavBar />
       <Header {...headerProps} />
       <main>{children}</main>
       <Footer />
